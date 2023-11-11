e291c731dc475ce1798bfeb8744df5bbb523b097
Ashik Andrews <121300448+ashikandrew@users.noreply.github.com>
2023-11-11 16:59:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Ashi's Llama 2 Chatbot')
+    st.title('🦙💬 Ashi\'s Llama 2 Chatbot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
